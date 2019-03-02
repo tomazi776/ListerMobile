c44eeae5547088713ba74c2bd56985a27218a374
tom <urban776@gmail.com>
2019-03-02 20:05:14 +0100

    Add models

---
+++ ListerMobile/ListerMobile/Models/ShoppingList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListerMobile.Models
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "DayOfWeek";
+        public DateTime CreationDate { get; set; }
+        public string Body { get; set; }
+        public User User { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+        //public ShoppingListStatus Status { get; set; } = ShoppingListStatus.Draft;
+
+        //public enum ShoppingListStatus
+        //{
+        //    Archived, Draft, Received, Saved, Sent
+        //}
+    }
+}
