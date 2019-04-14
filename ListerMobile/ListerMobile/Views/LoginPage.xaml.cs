0d46a63a285e0a6efbb8932c3b8c8c5d620004ee
tomasz <urban776@gmail.com>
2019-04-14 15:15:45 +0200

    Add LoginPage and locker icon

---
+++ ListerMobile/ListerMobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListerMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Button_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
